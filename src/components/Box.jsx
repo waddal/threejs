f24723d33a29510fd7ae208024440e408553c708
scriptsalmon@gmail.com <scriptsalmon@gmail.com>
2022-04-12 23:41:14 -0700

    box changes colors ooo

--- src/components/Box.jsx
+++ src/components/Box.jsx
@@ -1,10 +1,21 @@
+import { useState } from "react";
+
 const Box = () => {
   // --> https://threejs.org/docs/index.html#api/en/geometries/BoxGeometry
+  const initialColor = "#CF1F55";
+  const [color, setColor] = useState(initialColor);
+
+  const handleDown = () => [setColor("#CCCCFF")];
+  const handleUp = () => [setColor(initialColor)];
 
   return (
-    <mesh position={[0, 0, 0]}>
+    <mesh
+      position={[0, 0, 0]}
+      onPointerUp={handleUp}
+      onPointerDown={handleDown}
+    >
       <boxBufferGeometry attach="geometry" args={[1, 1, 1]} />
-      <meshStandardMaterial attach="material" color="#CF1F55" border="black" />
+      <meshStandardMaterial attach="material" color={color} border="black" />
     </mesh>
   );
 };
